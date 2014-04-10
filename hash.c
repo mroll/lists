ac76a9a59aa85fdc1cc67032728e1311ade616d0
mroll <mpr56@drexel.edu>
2014-04-10 13:38:02 -0400

    Jenkins test on hash.c again

--- hash.c
+++ hash.c
@@ -4,7 +4,7 @@
 #include "list.h"
 #include "hash.h"
 
-//Jenkins test comment
+//Jenkins test comment again
 
 HashTab* hashtab_init(int width, int nsize, HashFunc hash, HashCmp cmp) {
 	HashTab *tab = (HashTab *)malloc(sizeof(HashTab));
